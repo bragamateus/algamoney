d18af8cd10f726240e27bdcd13ab733a3324507e
bragamateus <mateus.braga@basis.com.br>
2020-03-29 23:29:48 -0300

    desafio criar pessoa

--- algamoney-api/src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -1,11 +1,21 @@
 package com.example.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoneyapi.model.Pessoa;
 import com.example.algamoneyapi.repository.PessoaRepository;
@@ -22,4 +32,23 @@ public class PessoaResource {
 		return pessoaRepository.findAll();
 	}
 
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Pessoa> buscarPorId(@PathVariable Long codigo) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		if (!pessoa.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(pessoa.get());
+	}
 }
